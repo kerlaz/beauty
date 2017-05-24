28975da5ba6ebcb6c19b5e069a73a6bb67c59597
Yerlan Bakuyev <yerlan.bakuyev@gmail.com>
2017-05-24 08:21:27 +0600

    12

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/thelist', function (req, res, next) {
+router.get('/masters', function (req, res, next) {
     var MongoClient = mongodb.MongoClient;
 
     var url = 'mongodb://localhost:27017/inkodabeauty';
@@ -35,10 +35,45 @@ router.get('/thelist', function (req, res, next) {
                 } else if (result.length){
                     console.log(result);
                     res.render('masterlist',{
-                        "masterlist":result
+                        masterlist: result,
+                        title: "Masters"
                     });
                 } else {
                     res.send('No document found');
+                }
+                db.close();
+            })
+        }
+    })
+});
+
+router.get('/newmaster', function (req, res, next) {
+    res.render('newmaster',{title: 'Add master'})
+});
+
+router.post('/addmaster', function (req, res, next) {
+    var MongoClient = mongodb.MongoClient;
+
+    var url = 'mongodb://localhost:27017/inkodabeauty';
+
+    MongoClient.connect(url,function (err, db) {
+        if (err) {
+            console.log('Unable to connect to the server.')
+        } else {
+            console.log('Connection established');
+
+            var masters = db.collection('masters');
+
+            var master1 = {
+                name: req.body.master,
+                services: [],
+                shedule: []
+            };
+            masters.insertOne(master1,null,function (err, result) {
+                if(err){
+                    console.log(err)
+                } else {
+                    res.redirect("masters");
                 }
                 db.close();
             })
